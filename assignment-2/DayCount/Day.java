8cdaf50f413aa7e11d034f37cfa5ffd0c149eb1e
Ank1taS <sahoosona2209@gmail.com>
2021-01-14 20:01:10 +0530

    typo corrected

--- assignment-2/DayCount/Day.java
+++ assignment-2/DayCount/Day.java
@@ -25,22 +25,22 @@ class Calculate
     {
         if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
         {
-            System.out.println("Total days in the moth of " + month + " and " + year + " are  31.");
+            System.out.println("Total days in the month of " + month + " and " + year + " are  31.");
         } 
         else if (month == 4 || month == 6 || month == 9 || month == 11)
         {
-            System.out.println("Total days in the moth of " + month + " and " + year + " are  30.");
+            System.out.println("Total days in the month of " + month + " and " + year + " are  30.");
         } 
         else    //for february month
         {
             if(leap())
             {
-                System.out.println("Total days in the moth of " + month + " and " + year + " are  29.");
+                System.out.println("Total days in the month of " + month + " and " + year + " are  29.");
  
             }
             else
             {
-                System.out.println("Total days in the moth of " + month + " and " + year + " are  28.");
+                System.out.println("Total days in the month of " + month + " and " + year + " are  28.");
 
             }
         }
